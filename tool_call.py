6d52dce760503f04d6c0b1003ebcbb863616f505
aryan <aryantyagi760@gmail.com>
2026-06-12 15:56:08 +0530

    memory added

--- tool_call.py
+++ tool_call.py
@@ -6,6 +6,9 @@ from langgraph.graph.message import add_messages
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.tools import tool
 
+from langgraph.checkpoint.memory import MemorySaver
+
+
 from langgraph.prebuilt import ToolNode,tools_condition
 
 class State(TypedDict):
@@ -35,14 +38,22 @@ builder.add_node("tools",ToolNode(tools))
 builder.add_edge(START,"chatbot")
 builder.add_conditional_edges("chatbot",tools_condition)
 builder.add_edge("tools","chatbot")  # after tool runs, go back to chatbot
+#memory added
+memory=MemorySaver()
 
-graph=builder.compile()
+graph=builder.compile(checkpointer=memory)
 print(display(Image(graph.get_graph().draw_mermaid_png())))
 print(graph.get_graph().draw_mermaid())
-state=graph.invoke({"messages":[{"role": "user", "content": "i want to buy 20 GOOG stocks using current price.Then 10 apple. what will be the total cost?"}]})
+
+#first thread
+config={"configurable":{"thread_id":"1"}}
+state=graph.invoke({"messages":[{"role": "user", "content": "i want to buy 20 GOOG stocks using current price.Then 10 apple. what will be the total cost?"}]},config=config)
 print(state["messages"][-1].content)
 
-
+#same thread - continues the conversation with memory
+config={"configurable":{"thread_id":"1"}}
+state=graph.invoke({"messages":[{"role": "user", "content": "what is the total cost now"}]},config=config)
+print(state["messages"][-1].content)
 
 
 
